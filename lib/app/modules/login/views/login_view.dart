d1af1f2372a02861b0a7b85d5be7215f9b23d074
anjeliputeri <anjeliputeri92@gmail.com>
2023-08-20 14:37:30 +0700

    delete

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
-//
+
 class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
